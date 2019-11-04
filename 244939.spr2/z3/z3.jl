9f980c21a8fd877188eb9aa1522bdf96faeedeac
Daniel <danieldr1212@gmail.com>
2019-11-04 22:21:40 +0100

    xD

--- 244939.spr2/z3/z3.jl
+++ 244939.spr2/z3/z3.jl
@@ -6,42 +6,34 @@ include("hilb.jl")
 include("matcond.jl")
 
 # A - matrix, b - vector machx - calculated vector, x - real vector
-
-gaussian_elimination(A,b) = A\b
-inversion(A,b) = inv(A)*b
-approx_error(machx, x) = norm(machx - x) / norm(x)
+gauss(A,b) = A\b
+inver(A,b) = inv(A)*b
+approx_err(machx, x) = norm(machx - x) / norm(x)
 
 function print_result(T,A,n)
+    #create vector x with ones
     x = ones(T, n)
     b = A*x
-    gauss_x = gaussian_elimination(A,b)
-    inv_x = inversion(A,b)
-    gauss_err = approximation_error(gauss_x,x)
-    inv_err = approximation_error(inv_x,x)
-
-    # println("$(n)x$(n) & $(rank(A)) & $(cond(A)) & $(gauss_err) & $(inv_err) \\\\")
-    # @printf("%dx%d & %d & %.15e & %.15e & %.15e \\\\\n",n,n,rank(A),cond(A),gauss_err,inv_err)
-    println("Size: $(n)x$(n)\tRank: $(rank(A))")
-    println("Cond: $(cond(A))")
-    println("Gauss error: $(gauss_err)\nInversion error: $(inv_err)\n")
+    gauss_x = gauss(A,b)
+    inv_x = inver(A,b)
+    gauss_err = approx_err(gauss_x,x)
+    inv_err = approx_err(inv_x,x)
+    print(" Size:= $(n)x$(n) = Rank = $(rank(A))")
+    print(" =Cond: = $(cond(A)) =")
+    print(" Gauss error: = $(gauss_err) =Inversion error:=  $(inv_err) =\n")
 end
 
 T = Float64
-# maximum size for Hilbert's matrix
-matrix_max = 20
-# conds for random matrix
-c = [T(1), T(10), T(10^3), T(10^7), T(10^12), T(10^16)]
-# sizes for random matrix
-n = [5, 10, 20]
 
-# Running program for Hilbert's matrices of different sizes
-println("Hilbert Matrix\n")
-for i = 1: matrix_max
+println("Hilbert Matrix")
+for i = 3: 20
     print_result(T,hilb(i),i)
 end
 
-# Running program for random matrices of different size and cond
-println("\n\nRandom Matrix\n")
+c = [T(1), T(10), T(10^3), T(10^7), T(10^12), T(10^16)]
+n = [5, 10, 20]
+
+println("Random Matrix")
 for i = 1: length(n)
     for j = 1: length(c)
         print_result(T, matcond(n[i],c[j]), n[i])
